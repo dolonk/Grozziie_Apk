25826204e41764e834ef634b073ab84acdb7dc70
dolon <dolonk9@gmail.com>
2023-07-21 19:53:30 +0600

    Update Add provider dolon

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,7 @@ void main() async{
   runApp( const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return ScreenUtilInit(
-//       designSize: const Size(360, 640),
-//       minTextAdapt: true,
-//       splitScreenMode: true,
-//       builder: (BuildContext context, Widget? child) {
-//         return MaterialApp(
-//           title: 'Grozziie',
-//           debugShowCheckedModeBanner: false,
-//           theme: ThemeData(
-//             primarySwatch: Colors.blue,
-//           ),
-//           home: child,
-//         );
-//       },
-//       child: CreateLabel(),
-//     );
-//   }
-// }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -62,7 +41,7 @@ class MyApp extends StatelessWidget {
           ),
         );
       },
-      child: CreateLabel(),
+      child: const CreateLabel(),
     );
   }
 }
